87b85984200d4dc06c3912b31208237b2bc606b0
Chad Duffey <chadduffey@hotmail.com>
2016-11-06 15:31:29 -0800

    venv .gitignore

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from posts import app
 
 def run():
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
