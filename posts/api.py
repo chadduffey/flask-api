5f2d11a5e16fc24cbf55e397f84d134a55e51b63
Chad Duffey <chadduffey@hotmail.com>
2016-11-07 16:24:16 -0800

    add decorator for valid post input + schema for json

--- posts/api.py
+++ posts/api.py
@@ -8,12 +8,27 @@ from . import decorators
 from posts import app
 from .database import session
 
+# JSON Schema describing the structure of a post
+post_schema = {
+    "properties": {
+        "title" : {"type" : "string"},
+        "body": {"type": "string"}
+    },
+    "required": ["title", "body"]
+}
+
 
 @app.route("/api/posts", methods=["GET"])
 @decorators.accept("application/json")
 def posts_get():
     """Get a list of posts"""
-    posts = session.query(models.Post).order_by(models.Post.id)
+    title_like = request.args.get("title_like")
+
+    posts = session.query(models.Post)
+
+    if title_like:
+        posts = posts.filter(models.Post.title.contains(title_like))
+    posts = posts.order_by(models.Post.id)
 
     data = json.dumps([post.as_dictionary() for post in posts])
 
@@ -53,3 +68,33 @@ def post_delete(id):
 
     data = json.dumps(post.as_dictionary())
     return Response(data, 200, mimetype="application/json")
+
+
+@app.route("/api/posts", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_post():
+    """ Add a new post """
+    #print(request)
+    data = request.json
+
+    # Check that the JSON supplied is valid
+    # If not you return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    # Add the post to the database
+    post = models.Post(title=data["title"], body=data["body"])
+    session.add(post)
+    session.commit()
+
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
+
